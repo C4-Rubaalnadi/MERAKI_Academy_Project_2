00e7c91c0e8b2944c880a32c90961af36d83cccd
Rubaalnadi <rubaalnadi5@gmail.com>
2021-12-10 13:51:59 +0200

    make when i add to add chart add 1 to chart shop

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const body = $("body");
 const divMain = $("<div> </div>");
 const logo = $("<h1> Book Store </h1>");
-const icon = $(`<i class="fas fa-cart-plus"></i>`);
-const cartContanier = $('<div> </div>');
-cartContanier.addClass('cartContainer');
-icon.appendTo(divMain);
+const divChart = $(`<div></div>`);
+divChart.addClass('divChart');
+// const icon = $(`<i class="fas fa-cart-plus"></i>`);
+let add = 0;
+let chart = $(`<span> ${add} </span>`);
+chart.appendTo(divMain);
+const cartContanier = $("<div> </div>");
+cartContanier.addClass("cartContainer");
+//icon.appendTo(divMain);
 logo.appendTo(divMain);
 divMain.appendTo(body);
 const book =
@@ -14,6 +19,7 @@ const book =
     // cart  [
     {
       img: "book.jpg",
+      alt : 'book1',
       title: "book1",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, rem. Minimavoluptatibus corrupti et qui!",
@@ -22,6 +28,7 @@ const book =
     },
     {
       img: "book.jpg",
+      alt : 'book2',
       title: "book2",
       description: "aaaaaaaaaaa",
       price: "4$",
@@ -29,6 +36,7 @@ const book =
     },
     {
       img: "book.jpg",
+      alt : 'book3',
       title: "book3",
       description: "Lorem ipsum dolor sit amet  corrupti et qui!",
       price: "4$",
@@ -36,23 +44,42 @@ const book =
     },
     //  ]
   ];
+class Chart{
+constructor(img,bookName,bookPrice){
+  this.img = img;
+  this.bookName = bookName;
+  this.bookPrice = bookPrice;
+}
+getInfo(){
+  const img = $(`<img>`);
+  const bookTitle = $(`<p></p>`);
+  const book_price = $(`<span></span>`);
+  bookTitle
+}
 
-book.forEach((obj,index) => {
+}
+
+book.forEach((obj, index) => {
   const divCart = $("<div> </div>");
-  divCart.addClass(`cart${index}`)
+  divCart.addClass(`cart${index}`);
   const img = $("<img />");
   const description = $(`<p>${obj.description}<p>`);
   const price = $(`<span> ${obj.price} </span>`);
   const title = $(`<h3> ${obj.title} </h3>`);
   const btn = $(`<button> ${obj.add_to} </button>`);
-   img.attr("src",obj.img);
-   img.appendTo(divCart);
-   title.appendTo(divCart);
-   description.appendTo(divCart);
-   price.appendTo(divCart)
-   btn.appendTo(divCart)
+  img.attr("src", obj.img ).attr('alt',obj.alt);
+  //alt.attr('alt',obj.alt); 
+  img.appendTo(divCart);
+  title.appendTo(divCart);
+  description.appendTo(divCart);
+  price.appendTo(divCart);
+  btn.appendTo(divCart);
   divCart.appendTo(cartContanier);
   cartContanier.appendTo(body);
+  btn.on('click',() => {
+    add++;
+    chart.text(add);
+    });
 });
-cartContanier.css('display','flex');
-cartContanier.css('gap','5px');
+// cartContanier.css('display','flex');
+// cartContanier.css('gap','5px');
